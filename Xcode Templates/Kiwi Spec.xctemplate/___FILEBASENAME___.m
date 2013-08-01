453931fe1564000ca8d2b32cc284fa936f2d616d
Andy Kim <andy@verticalbrands.com>
2013-08-01 13:42:24 -0700

    Better Kiwi Xcode template

--- Xcode Templates/Kiwi Spec.xctemplate/___FILEBASENAME___.m
+++ Xcode Templates/Kiwi Spec.xctemplate/___FILEBASENAME___.m
@@ -1,13 +1,19 @@
-//
-//  ___FILENAME___
-//  ___PROJECTNAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
-//
-
 #import "Kiwi.h"
+#import "___VARIABLE_cutClass:identifier___.h"
+
+@interface ___VARIABLE_cutClass:identifier___ (Test)
+@end
 
 SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___)
+
+describe(@"___VARIABLE_cutClass:identifier___", ^{
+    beforeEach(^{
+    });
+    
+    describe(@"_REPLACE_ME_", ^{
+        it(@"_REPLACE_ME_, ^{
+        });
+    });
+});
 
 SPEC_END
